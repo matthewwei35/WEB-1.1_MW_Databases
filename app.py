206262385c87041db35fc5c0c9a4b6d3c314056c
Matthew Wei <matthewwei35@gmail.com>
2021-03-02 18:31:57 -0800

    feat(create a plant): completed create route

--- app.py
+++ app.py
@@ -40,16 +40,17 @@ def create():
         # TODO: Get the new plant's name, variety, photo, & date planted, and 
         # store them in the object below.
         new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
+            'name': request.form.get('plant_name'),
+            'variety': request.form.get('variety'),
+            'photo_url': request.form.get('photo'),
+            'date_planted': request.form.get('date_planted')
         }
         # TODO: Make an `insert_one` database call to insert the object into the
         # database's `plants` collection, and get its inserted id. Pass the 
         # inserted id into the redirect call below.
+        inserted_plant = mongo.db.plants.insert_one(new_plant)
 
-        return redirect(url_for('detail', plant_id=''))
+        return redirect(url_for('detail', plant_id=inserted_plant.inserted_id))
 
     else:
         return render_template('create.html')
